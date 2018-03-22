5168b9be74f5a265edc9456f9d8f851bb8dd5926
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2018-03-22 11:51:19 +0100

    Changed Gemfile for Heroku

--- app/models/grumble.rb
+++ app/models/grumble.rb
@@ -1,7 +1,7 @@
 class Grumble < ApplicationRecord
 	enum status: [:Pending, :Approved, :Rectified, :Satisfied]
 	belongs_to :category, foreign_key: :category_id
-	belongs_to :company
+	belongs_to :company, foreign_key: :company_id
 	belongs_to :grumbler, foreign_key: :grumbler_id
 	has_many :comments
 	mount_uploader :file, FileUploader
--- db/migrate/20180313161054_add_company_id_to_grumbles.rb
+++ db/migrate/20180313161054_add_company_id_to_grumbles.rb
@@ -1,5 +1,5 @@
 class AddCompanyIdToGrumbles < ActiveRecord::Migration[5.1]
   def change
-    add_reference :grumbles, :company, foreign_key: true
+    add_column :grumbles, :company_id, :integer
   end
 end
